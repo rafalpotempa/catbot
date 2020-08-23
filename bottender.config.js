9e9a7c3661c7ac7c978b9c7f7088136a4fad403e
Rafal Potempa <rafal.potem@gmail.com>
2020-08-23 18:59:14 +0200

    Set up catbot

---
+++ bottender.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  session: {
+    driver: 'memory',
+    stores: {
+      memory: {
+        maxSize: 500,
+      },
+    },
+  },
+  initialState: {},
+  channels: {
+    messenger: {
+      enabled: true,
+      path: '/webhooks/messenger',
+      pageId: process.env.MESSENGER_PAGE_ID,
+      accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+      appId: process.env.MESSENGER_APP_ID,
+      appSecret: process.env.MESSENGER_APP_SECRET,
+      verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+      catApiToken: process.env.CAT_API_TOKEN,
+    },
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+module.exports = async function App(context) {
+
+	if (context.event.isText && context.event.text.includes('/meow')) {
+		getCat().then(cat => context.sendImage(cat));	
+	}
+};
+
+async function getCat() {
+	const axios = require('axios');
+	try {
+		let res = await axios({
+			method: 'get',
+			url: 'https://api.thecatapi.com/v1/images/search',
+			headers: {'x-api-key': process.env.CAT_API_TOKEN},
+			params: {'params': {'limit': 1, 'size': "full"}}	
+		})
+		return res.data['0'].url
+	} catch (error) {
+		console.log(error)
+	}
+}
